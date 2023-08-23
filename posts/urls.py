74ae40c1c10899b6499d5ba7e50283c989b43f83
sngw8k <jso0304@naver.com>
2023-08-23 14:04:29 +0900

    Create 기능 구현

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    
+    # Read
     path('', views.index, name='index'),
+    path('<int:id>/', views.detail, name='detail'),
 
+    # Create
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 # Create your views here.
@@ -12,3 +12,31 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def detail(request, id):
+    
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'detail.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # post = Post()
+    # post.title = title
+    # post.content = content
+    # post.save()
+
+    post = Post(title=title, content=content)
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
+
